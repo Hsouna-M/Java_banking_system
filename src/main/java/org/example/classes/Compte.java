2a2c54cd9168833b9280e3eb413ff35d41839455
Mahdy Merdacy <ynonymehdi02@gmail.com>
2025-06-07 20:16:55 +0100

    logic Compte

--- src/main/java/org/example/classes/Compte.java
+++ src/main/java/org/example/classes/Compte.java
@@ -1,5 +1,63 @@
 package org.example.classes;
 
+import java.util.Date;
+
 public abstract class Compte {
-    public abstract boolean retirer();
+    private int RIB;
+    private String type;
+    private double solde;
+    private boolean estBlockee;
+    private Date dateOuverture;
+
+    public Compte(int RIB, String type) {
+        this(RIB, type, 0.0, false); // default solde = 0.0, estBlockee = false
+    }
+
+
+    public Compte(int RIB, String type, double solde, boolean estBlockee) {
+        this.RIB = RIB;
+        this.type = type;
+        this.solde = solde;
+        this.estBlockee = estBlockee;
+        this.dateOuverture = new Date();
+    }
+
+    public int getRIB() {
+        return RIB;
+    }
+
+    public void setRIB(int RIB) {
+        this.RIB = RIB;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public boolean retirer(double montant) {
+        if (montant > solde) {
+            System.out.println("Vous ne disposez pas de ce montant.\nVeuillez insérer un montant équivalent.");
+            return false;
+        } else {
+            solde -= montant;
+            return true;
+        }
+    }
+
+    public abstract void afficherInfos(); // Abstract method
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "RIB=" + RIB +
+                ", type='" + type + '\'' +
+                ", solde=" + solde +
+                ", estBlockee=" + estBlockee +
+                ", dateOuverture=" + dateOuverture +
+                '}';
+    }
 }
--- src/main/java/org/example/classes/CompteEpargne.java
+++ src/main/java/org/example/classes/CompteEpargne.java
@@ -1,8 +1,35 @@
 package org.example.classes;
 
-public class CompteEpargne extends Compte{
+import java.util.Date;
+import java.util.Calendar;
+
+public class CompteEpargne extends Compte {
+    private static final double TAUX_INTERET = 0.05;
+
+    public CompteEpargne(int RIB, String type) {
+        super(RIB, "Epargne");
+    }
+
     @Override
-    public boolean retirer() {
-        return false;
+    public boolean retirer(double montant) {
+        if (montant > getSolde()) {
+            System.out.println("Fonds insuffisants pour un retrait d'un compte épargne.");
+            return false;
+        } else {
+            setSolde(getSolde() - montant);
+            return true;
+        }
+    }
+
+    public void appliquerInteretAnnuel() {
+        // Ajoute 5% d'intérêt au solde
+        double interet = getSolde() * TAUX_INTERET;
+        setSolde(getSolde() + interet);
+        System.out.println("Intérêts annuels appliqués : +" + interet + " TND");
+    }
+
+    @Override
+    public void afficherInfos() {
+        System.out.println(this.toString() + " (Compte Épargne avec intérêts à 5%)");
     }
 }
